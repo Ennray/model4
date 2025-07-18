c56d2494a65fbc3d60dcdca8d2a8a47b2ab32e9e
Ennray <1468339242@qq.com>
2025-07-18 16:22:55 +0800

    封装数据以便于后续调用

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-
+import json
 
 def save_uav_multi_positions(
     uavs_speed,
@@ -7,30 +7,25 @@ def save_uav_multi_positions(
     enemy_init_positions,
     our_pre_turn_positions,
     our_post_turn_positions,
-    filename="uav_full_path_record.txt"
+    filename="uav_full_path_record.json"
 ):
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(f"我方速度设置:\n当前速度：{uavs_speed[0]}   加速度：{uavs_speed[1]}   减速度：{uavs_speed[2]}  最大速度：{uavs_speed[3]}  转弯速度：{uavs_speed[4]}\n")
-        f.write(f"敌方速度设置：{enemy_speed}\n\n\n")
+    data = {
+        "uavs_speed": {
+            "current": uavs_speed[0],
+            "acceleration": uavs_speed[1],
+            "deceleration": uavs_speed[2],
+            "max_speed": uavs_speed[3],
+            "turn_speed": uavs_speed[4]
+        },
+        "enemy_speed": enemy_speed,
+        "our_initial_positions": our_init_positions,
+        "enemy_initial_positions": enemy_init_positions,
+        "our_pre_turn_positions": our_pre_turn_positions,
+        "our_post_turn_positions": our_post_turn_positions
+    }
 
-        f.write("我方初始位置\n")
-        for i in range(len(our_init_positions)):
-            f.write("{:<16} {:<16} {:>8}\n".format(*our_init_positions[i]))
-        f.write("\n\n")
-
-        f.write("敌方初始位置\n")
-        for i in range(len(enemy_init_positions)):
-            f.write("{:<16} {:<16} {:>8}\n".format(*enemy_init_positions[i]))
-        f.write("\n\n")
-
-        f.write("我方转弯前位置\n")
-        for i in range(len(our_pre_turn_positions)):
-            f.write("{:<16} {:<16} {:>8}\n".format(*our_pre_turn_positions[i]))
-        f.write("\n\n")
-
-        f.write("我方转弯后位置\n")
-        for i in range(len(our_post_turn_positions)):
-            f.write("{:<16} {:<16} {:>8}\n".format(*our_post_turn_positions[i]))
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 
 
